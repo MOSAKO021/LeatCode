5a3b3bbd94d28000a01f43f5f8a2c16244b76cfa
the Cyber Sage <141695095+MOSAKO021@users.noreply.github.com>
2024-12-26 17:02:48 +0530

    Update App.jsx

--- frontend-vercel/src/App.jsx
+++ frontend-vercel/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
     try {
       setIsLoading(true);  // Start loading when the request is made
 
-      const response = await axios.get(`http://localhost:3000/${userName}`);
+      const response = await axios.get(`https://leetcode-back.vercel.app/${userName}`);
       
       setUserData(response.data);
     } catch (error) {
