a4d23da655a179f4f6ea651f3457235771b8a6ef
theCyberSage <ultralegend021@gmail.com>
2024-12-26 22:34:26 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const { log } = require('console');
 
 app.use(cors());
 
@@ -46,7 +47,7 @@ const isValidUsername = async (username) => {
 
 app.get('/:username', async (req, res) => {
   const username = req.params.username; 
-
+  console.log('Fetching data for user:', username);
   // Step 1: Check if the username is valid
   const validUser = await isValidUsername(username);  
   if (!validUser) {
